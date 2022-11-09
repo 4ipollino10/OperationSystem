8487e5fbfc923a064a8d4cdd56f55c50cbea29f2
Timur Gulyaev <92777819+4ipollino10@users.noreply.github.com>
2022-11-09 13:53:48 +0700

    Update SleepSort(lab6).c

--- SleepSort(lab6).c
+++ SleepSort(lab6).c
@@ -6,7 +6,7 @@
 #include <errno.h>
 
 #define MAX_STR_AMOUNT 100
-#define COFFICIENT = 100000
+#define COFFICIENT 100000
 
 #define SUCCESSFULL_PROGRAMM_EXIT_CODE 0
 #define SUCCESSFULL_THREAD_CREATION_CODE 0
@@ -22,7 +22,7 @@ void* printString(void* arg){
     int status = usleep(COFFICIENT * strlen((char *)arg));
     
     if(status != SUCCESSFULL_SLEEP_CODE){
-        fprintf(stderr, "usleep() error, status: %d, error: %s\n", strerror(status));
+        fprintf(stderr, "usleep() error, status: %d, error: %s\n", status, strerror(status));
     }
 
     printf("%s",(char *)arg);
@@ -40,10 +40,22 @@ int readStrings(char** strings, int* strCounter){
     int readSymbols = 2;
     int strIndex = 0;
     
-    size_t strLength = 0;
+    size_t strLength = 100;
 
+    printf("Type your string here.\npress 'Enter' to start sort")
+    
     while(readSymbols > 1 && strIndex < 100){
-        int readSymbols = getline(strings[strIndex], &strLength, stdin);
+        strings[strIndex] = (char *)malloc(sizeof(char) * strLength);
+        
+        if(strings[strIndex] == NULL){
+            fprtinf(stderr, "malloc() error: %s\n", strerror(errno));
+            
+            *strCounter = strIndex;
+            
+            return ERROR_READ_CODE;
+        }
+        
+        readSymbols = getline(&strings[strIndex], &strLength, stdin);
        
         if(readSymbols == ERROR_GETLINE_CODE){
             fprintf(stderr, "getline() error: %s\n", strerror(errno));
@@ -85,7 +97,7 @@ int main(){
     }
 
     for(int i = 0; i < strCounter; ++i){
-        int status = phread_create(&threads[i], NULL, printString, strings[i]);
+        int status = pthread_create(&threads[i], NULL, printString, strings[i]);
         
         if(status != SUCCESSFULL_THREAD_CREATION_CODE){
             fprintf(stderr, "Failed to create thread, status: %d, error: %s\n", status, strerror(status));
